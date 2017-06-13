75cd9be25c9466201b6787ff9e22af359e8d57d4
S <S@mail.minecraft.jp.net>
2017-06-13 22:25:57 +0900

    Fix: カッコののぞかれ方がおかしいのを修正

--- generate.js
+++ generate.js
@@ -50,7 +50,7 @@ var suffix = "わかったかはよ垢消せ";
 var maxLen = 140 - prefix.length - suffix.length;
 var textFormat = function(text){
   var text = text.replace(/\n/g, "");
-  var text = text.replace(/[(（].+[)）]([^。]*(?:。|$))/, "$1");
+  var text = text.replace(/[(（][^)）]+[)）]([^。]*(?:。|$))/, "$1");
   if(text.length <= maxLen) {
     return prefix+text+suffix;
   }
